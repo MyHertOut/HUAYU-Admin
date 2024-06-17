1a7ebc30bb062a7f61c0aa89d4d7c94622704c49
MyHertOut <1483869553@qq.com>
2024-06-18 00:00:29 +0800

    feat：项目初始

---
+++ src/layouts/components/Footer/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="footer flx-center">
+    <a href="https://www.thermalmaxter.com/" target="_blank">Copyright © 2024 重庆华裕胜久材料有限公司</a>
+    <a href="https://beian.miit.gov.cn/" target="_blank">渝ICP备2023017748号-1</a>
+  </div>
+</template>
+
+<style scoped lang="scss">
+@import "./index.scss";
+</style>
